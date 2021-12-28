3ebd7ae8a5eea757a564ff4913c2205b2fcb46dd
Ana Beatriz <anabiawalker@gmail.com>
2021-12-28 15:12:20 -0300

    Adc Componentes Todo e About

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,9 +3,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import About from '../about/about'
+import Todo from '../todo/todo'
 
 export default props => ( //função arrow com expressão
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo></Todo>
+        <About></About>
     </div>
 )
