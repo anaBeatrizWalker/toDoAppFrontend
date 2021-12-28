614bab59ebe1ead4bad9e44f61793275dd9ddeea
Ana Beatriz <anabiawalker@gmail.com>
2021-12-28 15:32:07 -0300

    Adc Componente PageHeader

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,11 +1,12 @@
 //Componente que centraliza a maior parte das regras relativas ao cadastro de tarefas
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Todo extends Component{
     render(){
         return(
             <div>
-                <h1>ToDo</h1>
+                <PageHeader name='Tarefas' small='Cadastro'/>
             </div>
         )
     }
