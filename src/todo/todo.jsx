3ebf691cd792c4d465fbf515c5c1fedb4b6252fa
Ana Beatriz <anabiawalker@gmail.com>
2021-12-28 15:46:05 -0300

    Adc Componentes TodoForm e TodoList

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,12 +1,16 @@
 //Componente que centraliza a maior parte das regras relativas ao cadastro de tarefas
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component{
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
+                <TodoForm/>
+                <TodoList/>
             </div>
         )
     }
