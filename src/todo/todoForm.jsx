dbd80af4907e639279d0e5d6c50df422d8a89c38
Ana Beatriz <anabiawalker@gmail.com>
2021-12-30 12:46:31 -0300

    Adicionando Conexão com Redux

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => {
+const TodoForm = props => {
 
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
@@ -43,3 +45,6 @@ export default props => {
         </div>
     )
 }
+const mapStateToProps = state => ({description: state.todo.description})//todo.description vem de reducers.js
+
+export default connect(mapStateToProps)(TodoForm)
